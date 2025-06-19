57c8ac44373185cbaac847c0f59be1163d85100b
Mart121314 <128546404+Mart121314@users.noreply.github.com>
2025-06-19 15:42:35 +0200

    Use Blizzard API for live data

--- arenaq.gg/src/app/leaderboard/leaderboard.component.ts
+++ arenaq.gg/src/app/leaderboard/leaderboard.component.ts
@@ -1,11 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { LeaderboardApiService } from './leaderboardapi.service';
 
 @Component({
   selector: 'app-leaderboard',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule],
   templateUrl: './leaderboard.component.html',
   styleUrl: './leaderboard.component.css'
 })
-export class LeaderboardComponent {
+export class LeaderboardComponent implements OnInit {
+  players: any[] = [];
+  currentPage = 1;
 
+  constructor(private lbApi: LeaderboardApiService) {}
+
+  ngOnInit(): void {
+    this.loadPage(1);
+  }
+
+  loadPage(page: number): void {
+    if (page < 1) return;
+    this.lbApi.getPage(page).subscribe((data) => {
+      this.players = data;
+      this.currentPage = page;
+    });
+  }
 }
---
+++ arenaq.gg/src/app/player/playerapi.service.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@angular/core';
+import { Observable, map } from 'rxjs';
+import { WowApiService } from '../wow-api.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PlayerApiService {
+  constructor(private wow: WowApiService) {}
+
+  /** Fetch a character's equipment from Blizzard API */
+  getEquipment(realm: string, name: string): Observable<string[]> {
+    return this.wow
+      .getCharacterEquipment(realm, name)
+      .pipe(map((res) => (res.equipped_items || []).map((i: any) => i.item.name)));
+  }
+}
